7a9c3dc9fcfd6bad2167f3c20c1138ba19b886ad
EvanKards42 <Evankardos37@gmail.com>
2018-03-08 16:13:19 -0800

    FoodUI
    Finished Food UI. recycle view, card, view, search all created. needs backend

--- app/src/main/java/enigmastudios/fridgebuddy/ProducePricePrediction.java
+++ app/src/main/java/enigmastudios/fridgebuddy/ProducePricePrediction.java
@@ -55,7 +55,7 @@ public class ProducePricePrediction extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.search ) {
             return true;
         }
 
