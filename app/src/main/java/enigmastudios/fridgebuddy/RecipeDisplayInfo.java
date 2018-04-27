612ed5d868670b8ddc8f92a53940e0fe6a735e9d
jgaines2013 <jgaines2013@gmail.com>
2018-04-26 20:43:13 -0700

    Recipe
    cutting jpg
    fixed recipes

---
+++ app/src/main/java/enigmastudios/fridgebuddy/RecipeDisplayInfo.java
@@ -0,0 +1,17 @@
+package enigmastudios.fridgebuddy;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jerry on 4/26/18.
+ */
+
+public class RecipeDisplayInfo extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_food_info);
+    }
+}
